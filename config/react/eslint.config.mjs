89d7088348ad1313f12caf6d5b36e01aed0b643c
Peter Bouquet <pete@stackcrafters.com>
2025-04-24 14:50:37 +0100

    Update dependencies, fix react eslint config

--- config/react/eslint.config.mjs
+++ config/react/eslint.config.mjs
@@ -1,31 +1,31 @@
+import reactHooks from 'eslint-plugin-react-hooks';
+import { fixupPluginRules } from '@eslint/compat';
 import tsParser from '@typescript-eslint/parser';
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import reactPlugin from 'eslint-plugin-react'
-import reactHooksPlugin from 'eslint-plugin-react-hooks'
-import globals from "globals";
-import js from "@eslint/js";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-reactPlugin.configs.recommended.plugins = { reactPlugin }
-reactPlugin.configs.recommended.languageOptions = {
-  parserOptions: reactPlugin.configs.recommended.parserOptions
-}
-delete reactPlugin.configs.recommended.parserOptions
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
 
 export default [
-  // reactPlugin.configs.recommended,
-  // reactHooksPlugin.configs.recommended,
-  js.configs.recommended,
-  eslintPluginPrettierRecommended,
+  ...compat.extends('plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'),
   {
+    plugins: {
+      'react-hooks': fixupPluginRules(reactHooks)
+    },
+
     languageOptions: {
       parser: tsParser,
       ecmaVersion: 2020,
       sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.jest
-      },
+
       parserOptions: {
         ecmaFeatures: {
           jsx: true
@@ -41,18 +41,21 @@ export default [
 
     rules: {
       'prettier/prettier': 'warn',
-      'no-explicit-any': 'off',
-      'no-inferrable-types': 'off',
-      'no-unused-vars': ['warn', {
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_",
-        "caughtErrorsIgnorePattern": "^_"
-      }],
-      // 'react-hooks/rules-of-hooks': 'error',
-      // 'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'react/display-name': 'off',
-      // 'react/jsx-uses-react': 'off',
-      // 'react/react-in-jsx-scope': 'off'
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true
+        }
+      ],
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
     }
   }
 ];
